834620cf7b6dc6306fe81443f8286242f02b5b38
Snehal Singh <snehals.272@gmail.com>
2021-08-17 15:52:09 -0400

    resolved issue

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,16 +11,25 @@ class LinksController < ApplicationController
   # This action redirects tiny url to it's original url 
   def show
     @link = Link.find_by_token(params[:token]) 
-    redirect_to root if @link.nil?
-    @ip = request.remote_ip   #Fetch ip address of user
-    Visitor.update_visitors_count(@link, @ip)  #Increase visited user count
-    redirect_to @link.original_url
+    if @link.nil?
+      flash[:error] = "Invalid Token"
+      redirect_to root_path
+    else  
+      @ip = request.remote_ip   #Fetch ip address of user
+      Visitor.update_visitors_count(@link, @ip)  #Increase visited user count
+      redirect_to @link.original_url
+    end  
   end
 
   # This action shows information of visited counts of a url 
   def info
-    @link = Link.find_by_token(params[:token]) 
-    @users = Visitor.where(link_id: @link.id)
+    @link = Link.find_by_token(params[:token])
+    if @link.nil?
+      flash[:error] = "Invalid token" 
+      redirect_to lists_links_path 
+    else  
+      @users = Visitor.where(link_id: @link.id)
+    end  
   end  
 
   # This action lists all url generated in DB
